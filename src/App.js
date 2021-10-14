02c578cac6d86a48e533d46ed8815c303162cb60
Francisco Alves <alvesextra@gmail.com>
2021-10-13 23:00:52 -0300

    upload arquivos do projeto

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, world React</h1>
+        <p>Meu primeiro projeto</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
